a27fd41a800f0e814ec820270251dedf20543cbc
Lito Bumba <litocdbumba@gmail.com>
2024-08-23 01:45:13 -0300

    Create and configure Jvm Convention

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,14 +1,7 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    alias(libs.plugins.runique.jvm.library)
 }
 
 dependencies {
-
     implementation(libs.kotlinx.coroutines.core)
 }
