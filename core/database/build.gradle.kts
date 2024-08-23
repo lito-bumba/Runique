f25a4be309ceb77da649859eadd0848c16190f57
Lito Bumba <litocdbumba@gmail.com>
2024-08-23 01:20:07 -0300

    Create and configure Room Convention

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.runique.android.library)
+    alias(libs.plugins.runique.android.room)
 }
 
 android {
@@ -7,7 +8,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.org.mongodb.bson)
 
     implementation(projects.core.domain)
