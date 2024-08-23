9632e4faf62dd663f7c7b58533381556400dd52b
Lito Bumba <litocdbumba@gmail.com>
2024-08-23 02:08:53 -0300

    Create and configure Jvm Ktor Convention

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.runique.android.application.cmpose)
+    alias(libs.plugins.runique.jvm.ktor)
     alias(libs.plugins.mapsplatform.secrets.plugin)
 }
 
--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.runique.android.library)
+    alias(libs.plugins.runique.jvm.ktor)
 }
 
 android {
@@ -7,7 +8,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.timber)
 
     implementation(projects.core.domain)
